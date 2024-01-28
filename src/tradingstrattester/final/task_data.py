f29151b43d42a55f614120ab4fb56bee4833de04
justinfranken <justin272@gmx.de>
2024-01-28 20:53:51 +0100

    Created the 'task_data.py' function to download the data and store it in the bld folder.

--- src/tradingstrattester/final/task_data.py
+++ src/tradingstrattester/final/task_data.py
@@ -2,16 +2,16 @@
 
 import pytask
 from tradingstrattester.config import ASSETS, BLD
-from tradingstrattester.data_management.data_gathering import data_download
+from tradingstrattester.data_management.data_functions import data_download
 
 for asset in ASSETS:
+    """Download financial data and store it in the bld folder."""
 
     @pytask.task(id=asset)
     def task_download_1d_data(
         symbol=asset,
-        produces=BLD / "python" / "data" / f"{asset}_1d.pkl",
+        produces=BLD / "python" / "data" / f"1d_{asset}.pkl",
     ):
-        """Download financial data and store it in the bld folder."""
         data_1d = data_download(
             symbol,
             end_date="2024-01-01",
@@ -19,3 +19,27 @@ for asset in ASSETS:
             frequency="1d",
         )
         data_1d.to_pickle(produces)
+
+    @pytask.task(id=asset)
+    def task_download_5m_data(
+        symbol=asset,
+        produces=BLD / "python" / "data" / f"5m_{asset}.pkl",
+    ):
+        data_5m = data_download(
+            symbol,
+            end_date="2024-01-01",
+            frequency="5m",
+        )
+        data_5m.to_pickle(produces)
+
+    @pytask.task(id=asset)
+    def task_download_60m_data(
+        symbol=asset,
+        produces=BLD / "python" / "data" / f"60m_{asset}.pkl",
+    ):
+        data_60m = data_download(
+            symbol,
+            end_date="2024-01-01",
+            frequency="60m",
+        )
+        data_60m.to_pickle(produces)
