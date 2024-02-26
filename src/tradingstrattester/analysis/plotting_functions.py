c9e145d09e97b6dc76243d385a54dc3b9457dfcc
justinfranken <justin272@gmx.de>
2024-02-26 14:47:40 +0100

    Split up plotting functions into helper functions and included docstrings.

--- src/tradingstrattester/analysis/plotting_functions.py
+++ src/tradingstrattester/analysis/plotting_functions.py
@@ -9,9 +9,40 @@ from tradingstrattester.config import STRATEGIES
 
 
 def plot_asset_strategy(data, id, initial_depot_cash, depends_on):
+    """Plot depot value for different strategies and asset price in the form of
+    candlesticks.
+
+    Parameters:
+        data (DataFrame): The DataFrame containing asset opening, high, low, and closing data from the data_download() function.
+        id (str): The identifier for the asset including the ending "[...].pkl", e.g. "60m_DB.pkl".
+        initial_depot_cash (float): The initial depot cash value defined in the config.py file.
+        depends_on (list): A list of file paths to the simulated depots for each strategy.
+
+    Returns:
+        fig (go.Figure): The Plotly figure object containing annotated asset price candlesticks and simulated depot values for each strategy.
+
+    """
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-    # Add strategy traces
+    _add_strategy_traces(fig, data, id, depends_on)
+    _add_initial_depot_annotation(fig, initial_depot_cash)
+    _add_asset_candlesticks(fig, data, id)
+
+    _add_figure_layout_asset_strategy(fig, id)
+
+    return fig
+
+
+def _add_strategy_traces(fig, data, id, depends_on):
+    """Add strategy traces to the plot.
+
+    Parameters:
+        fig (go.Figure): The Plotly figure object.
+        data (DataFrame): The DataFrame containing asset opening, high, low, and closing data from the data_download() function.
+        id (str): The identifier for the asset including the ending "[...].pkl", e.g. "60m_DB.pkl".
+        depends_on (list): A list of file paths to the simulated depots for each strategy.
+
+    """
     depot_out = {}
     indicator = -1
     for strategy in STRATEGIES:
@@ -28,6 +59,15 @@ def plot_asset_strategy(data, id, initial_depot_cash, depends_on):
             secondary_y=True,
         )
 
+
+def _add_initial_depot_annotation(fig, initial_depot_cash):
+    """Add initial depot value as a horizontal line with annotations to the plot.
+
+    Parameters:
+        fig (go.Figure): The Plotly figure object.
+        initial_depot_cash (float): The initial depot cash value defined in the config.py file.
+
+    """
     fig.add_hline(secondary_y=True, y=initial_depot_cash, line_dash="dot")
 
     fig.add_annotation(
@@ -40,7 +80,16 @@ def plot_asset_strategy(data, id, initial_depot_cash, depends_on):
         ay=10,
     )
 
-    # Add asset candle sticks
+
+def _add_asset_candlesticks(fig, data, id):
+    """Add asset candlesticks to the plot.
+
+    Parameters:
+        fig (go.Figure): The Plotly figure object.
+        data (DataFrame): The DataFrame containing asset opening, high, low, and closing data from the data_download() function.
+        id (str): The identifier for the asset including the ending "[...].pkl", e.g. "60m_DB.pkl".
+
+    """
     fig.add_trace(
         go.Candlestick(
             x=data.index,
@@ -53,29 +102,73 @@ def plot_asset_strategy(data, id, initial_depot_cash, depends_on):
         secondary_y=False,
     )
 
-    # Add figure title
+
+def _add_figure_layout_asset_strategy(fig, id):
+    """Add layout to the asset strategy figure.
+
+    Parameters:
+        fig (go.Figure): The Plotly figure object.
+        id (str): The identifier for the asset including the ending "[...].pkl", e.g. "60m_DB.pkl".
+
+    """
     fig.update_layout(
         title_text=f"<b>{id.split('.')[0]} Asset Price and Depot Value for Different Strategies<b>",
         xaxis_rangeslider_visible=False,
     )
 
-    # Set x-axis title
     fig.update_xaxes(title_text="Dates")
-
-    # Set y-axes titles
     fig.update_yaxes(title_text="<b>Depot value</b>", secondary_y=True)
     fig.update_yaxes(title_text="<b>Asset price</b>", secondary_y=False)
 
-    return fig
-
 
 def plot_indicators(data, id, initial_depot_cash, depends_on):
+    """Plot indicators for different strategies and no strategy (i.e. investing all the
+    cash at the beginning of the investing period).
+
+    Parameters:
+        data (DataFrame): The DataFrame containing asset opening, high, low, and closing data from the data_download() function.
+        id (str): The identifier for the asset including the ending "[...].pkl", e.g. "60m_DB.pkl".
+        initial_depot_cash (float): The initial depot cash value defined in the config.py file.
+        depends_on (list): A list of file paths to the simulated depots for each strategy.
+
+    Returns:
+        fig (go.Figure): The Plotly figure object containing annotated indicator bars of all strategies and no strategy (i.e. investing all the cash at the beginning of the investing period).
+
+    """
     fig = go.Figure()
 
     start_units = math.floor(initial_depot_cash / data.Close[1])
     rest_cash = initial_depot_cash - start_units * data.Close[1]
 
-    # Add indicator for no strategy
+    _add_no_strategy_indicator(fig, data, start_units, rest_cash, initial_depot_cash)
+
+    _add_strategy_indicators(
+        fig,
+        data,
+        id,
+        depends_on,
+        start_units,
+        rest_cash,
+        initial_depot_cash,
+    )
+
+    _add_figure_layout_indicator(fig, id)
+
+    return fig
+
+
+def _add_no_strategy_indicator(fig, data, start_units, rest_cash, initial_depot_cash):
+    """Add indicator for no strategy (i.e. investing all the cash at the beginning of
+    the investing period).
+
+    Parameters:
+        fig (go.Figure): The Plotly figure object.
+        data (DataFrame): The DataFrame containing asset opening, high, low, and closing data from the data_download() function.
+        start_units (int): The number of units purchased at the start of the investing period.
+        rest_cash (float): The remaining cash after purchasing units at the beginning of the investing period.
+        initial_depot_cash (float): The initial depot cash value defined in the config.py file.
+
+    """
     fig.add_trace(
         go.Indicator(
             mode="number+gauge+delta",
@@ -117,7 +210,17 @@ def plot_indicators(data, id, initial_depot_cash, depends_on):
         ),
     )
 
-    # Add indicators for each strategy
+
+def _add_strategy_indicators(fig, id, depends_on, initial_depot_cash):
+    """Add indicators for each strategy.
+
+    Parameters:
+        fig (go.Figure): The Plotly figure object.
+        id (str): The identifier for the asset including the ending "[...].pkl", e.g. "60m_DB.pkl".
+        depends_on (list): A list of file paths to the simulated depots for each strategy.
+        initial_depot_cash (float): The initial depot cash value defined in the config.py file.
+
+    """
     depot_out = {}
     indicator = -1
     for index, strategy in enumerate(STRATEGIES, start=1):
@@ -183,15 +286,31 @@ def plot_indicators(data, id, initial_depot_cash, depends_on):
             ),
         )
 
+
+def _add_figure_layout_indicator(fig, id):
+    """Add layout to the indicator bar figure.
+
+    Parameters:
+        fig (go.Figure): The Plotly figure object.
+        id (str): The identifier for the asset including the ending "[...].pkl", e.g. "60m_DB.pkl".
+
+    """
     fig.update_layout(
         title_text=f"<b>Comparison of {id.split('.')[0]} Strategy Indicator Bars and Immediately Investing Approach (No strategy)<b>",
         xaxis_rangeslider_visible=False,
     )
 
-    return fig
-
 
 def _generate_intervals(num_intervals):
+    """Generate intervals for positioning indicators in a vertical arrangement.
+
+    Parameters:
+        num_intervals (int): The number of intervals to generate.
+
+    Returns:
+        list: A list of intervals represented as pairs of start and end values.
+
+    """
     total_gap = 0.09 * (num_intervals - 1)
     interval_width = (1 - total_gap) / num_intervals
     intervals = []
