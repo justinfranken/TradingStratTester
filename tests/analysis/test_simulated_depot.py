cec00ddaef0e90dad0762b946cc4ae4b852ce9a2
justinfranken <justin272@gmx.de>
2024-02-29 16:51:35 +0100

    Included more tests for the simulated_depot function.

--- tests/analysis/test_simulated_depot.py
+++ tests/analysis/test_simulated_depot.py
@@ -1,6 +1,10 @@
 """"Test for the simulating depot functions."""
 
-from tradingstrattester.analysis.simulated_depot import simulated_depot
+import pytest
+from tradingstrattester.analysis.simulated_depot import (
+    _handle_errors_in_sim_depot_config_vars,
+    simulated_depot,
+)
 from tradingstrattester.config import (
     _id,
     initial_depot_cash,
@@ -9,14 +13,22 @@ from tradingstrattester.config import (
     unit_var,
 )
 
+# Test simulated_depot outcomes
 
-# Test config.py input variables
-def test_simulated_depot_outcomes():
+signals_list = [[2, 2], [1, 1], [0, 0]]
+pattern_list = ["sell", "buy", "nothing"]
+
+
+@pytest.mark.parametrize(
+    ("signals", "pattern"),
+    zip(signals_list, pattern_list),
+)
+def test_simulated_depot_outcomes(signals, pattern):
     """Test if simulated_depot outcomes are as expected."""
-    # Testing sell signal
+
     signal = {}
     for id in _id:
-        signal[f"signal_{id}"] = [2, 2]
+        signal[f"signal_{id}"] = signals
     test_dict = {}
     test_dict["_simple_signal_gen"] = signal
     depot = simulated_depot(
@@ -28,55 +40,34 @@ def test_simulated_depot_outcomes():
         unit_strat,
         unit_var,
     )
-    for id in _id:
-        assert (
-            depot["unit_dict"][id.split(".")[0]][1]
-            <= depot["unit_dict"][id.split(".")[0]][0]
-        )
+
+    # Testing sell signal
+    if pattern == "sell":
+        for id in _id:
+            assert (
+                depot["unit_dict"][id.split(".")[0]][1]
+                <= depot["unit_dict"][id.split(".")[0]][0]
+            )
 
     # Testing buy signal
-    signal = {}
-    for id in _id:
-        signal[f"signal_{id}"] = [1, 1]
-    test_dict = {}
-    test_dict["_simple_signal_gen"] = signal
-    depot = simulated_depot(
-        test_dict,
-        "_simple_signal_gen",
-        _id,
-        initial_depot_cash,
-        start_stock_prct,
-        unit_strat,
-        unit_var,
-    )
-    for id in _id:
-        assert (
-            depot["unit_dict"][id.split(".")[0]][1]
-            >= depot["unit_dict"][id.split(".")[0]][0]
-        )
+    if pattern == "buy":
+        for id in _id:
+            assert (
+                depot["unit_dict"][id.split(".")[0]][1]
+                >= depot["unit_dict"][id.split(".")[0]][0]
+            )
 
     # Testing do nothing signal
-    signal = {}
-    for id in _id:
-        signal[f"signal_{id}"] = [0, 0]
-    test_dict = {}
-    test_dict["_simple_signal_gen"] = signal
-    depot = simulated_depot(
-        test_dict,
-        "_simple_signal_gen",
-        _id,
-        initial_depot_cash,
-        start_stock_prct,
-        unit_strat,
-        unit_var,
-    )
-    for id in _id:
-        assert (
-            depot["unit_dict"][id.split(".")[0]][1]
-            == depot["unit_dict"][id.split(".")[0]][0]
-        )
+    if pattern == "nothing":
+        for id in _id:
+            assert (
+                depot["unit_dict"][id.split(".")[0]][1]
+                == depot["unit_dict"][id.split(".")[0]][0]
+            )
 
-    # Test if initial depot value is correct
+
+def test_initial_depot_cash_in_simulated_depot():
+    """Test if start value of simulated_depot() is equal to initial_depot_cash."""
     signal = {}
     for id in _id:
         signal[f"signal_{id}"] = [0, 0]
@@ -86,13 +77,41 @@ def test_simulated_depot_outcomes():
         test_dict,
         "_simple_signal_gen",
         _id,
-        initial_depot_cash,
+        100,
         start_stock_prct,
         unit_strat,
         unit_var,
     )
     for id in _id:
-        assert depot["value_dict"][id.split(".")[0]][0] == initial_depot_cash
+        assert depot["value_dict"][id.split(".")[0]][0] == 100
 
 
 # Test configuration variables from config.py file
+
+type_error_inputs = [
+    # wrong initial_depot_cash
+    ["1000", 0.25, "fixed_trade_units", 0.1],
+    # wrong start_stock_prct
+    [1000, "0.25", "fixed_trade_units", 0.1],
+    # wrong unit_strat
+    [1000, 0.25, initial_depot_cash, 0.1],
+    # wrong unit_var
+    [1000, 0.25, "fixed_trade_units", "0.1"],
+]
+
+
+@pytest.mark.parametrize("type_inputs", type_error_inputs)
+def test_handle_errors_in_sim_depot_config_vars_type_error(type_inputs):
+    """Test if _handle_errors_in_sim_depot_config_vars raises correct type errors."""
+    with pytest.raises(TypeError):
+        _handle_errors_in_sim_depot_config_vars(type_inputs)
+
+
+def test_handle_errors_in_sim_depot_config_vars_value_errors():
+    with pytest.raises(ValueError):
+        _handle_errors_in_sim_depot_config_vars(-1000, 0.25, "fixed_trade_units", 0.1)
+        _handle_errors_in_sim_depot_config_vars(1000, -0.25, "fixed_trade_units", 0.1)
+        _handle_errors_in_sim_depot_config_vars(1000, 1.25, "fixed_trade_units", 0.1)
+        _handle_errors_in_sim_depot_config_vars(1000, 0.25, "test", 0.1)
+        _handle_errors_in_sim_depot_config_vars(1000, 0.25, "fixed_trade_units", -0.1)
+        _handle_errors_in_sim_depot_config_vars(1000, 0.25, "fixed_trade_units", 1.1)
